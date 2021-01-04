00094da92a0ce0b2f1ecdbfd1f05fee1c01426b8
Luke Evans <luke@Luke-Evans-MacBook-Pro.local>
2021-01-04 12:25:40 -0800

    examine byte, short, int, long

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
         int myValue = 10000;
 
         // determine range of primitive type, wrapperClass.constant
+        // 32 bits 2s complement
         int myMinIntValue = Integer.MIN_VALUE;
         int myMaxIntValue = Integer.MAX_VALUE;
 
@@ -17,6 +18,27 @@ public class Main {
         System.out.println(overflowMaxValue);
 
         // int test = 2147483648; will throw error
+
+        // 8 bits
+        byte myMinByte = Byte.MIN_VALUE;
+        byte myMaxByte = Byte.MAX_VALUE;
+
+        System.out.println("byte min: " + myMinByte + " byte max: " + myMaxByte );
+
+        // 16 bits
+        short myMinShort = Short.MIN_VALUE;
+        short myMaxShort = Short.MAX_VALUE;
+
+        System.out.println("short min: " + myMinShort + " short max: " + myMaxShort);
+
+        // 64 bits
+        long myLongValue = 100L; // use L when assigning long. Default numeric literal is treated by int in java
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println("long min: " + myMinLongValue + " long max: " + myMaxLongValue);
+
 
     }
 }
