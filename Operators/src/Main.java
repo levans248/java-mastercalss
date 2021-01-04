087687093f8e56ee6b197617bdcc80b541632f80
Luke Evans <luke@Luke-Evans-MacBook-Pro.local>
2021-01-04 13:19:47 -0800

    java operators intro

---
+++ Operators/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result; // primitives are not assigned as references
+        System.out.println("Previous result: " + previousResult);
+
+        result -=1;
+
+        System.out.println("result: " + result);
+
+        result *= 10;
+
+        System.out.println(result);
+
+        result /= 5;
+
+        System.out.println(result);
+
+        result %= 3;
+        System.out.println(result);
+
+    }
+}
