ddc7df4d4fe7b9bda9ee344ae5897331a221981f
Luke Evans <luke@Luke-Evans-MacBook-Pro.local>
2021-01-04 13:00:23 -0800

    char and boolean types

---
+++ CharBoolean/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // char is unicode, 16 bits to make internationalization easy
+        char myChar = 'd';
+        char myUnicode = '\u00A9'; // precede unicode with \u
+
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+
+        // boolean can be either true or false
+        boolean myTrue = true; // lowercase t
+        boolean myFalse = false; // lowercase f
+
+        
+
+    }
+}
